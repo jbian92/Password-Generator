8d4d2619168da660b826155318a7774bde51c50e
Julia Bian <jujuzb88@gmail.com>
2021-07-14 19:15:41 +0000

    generates a lowercase, letter-only password given a length

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-# ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
 
+import functions
 
 # -- Initialization section --
 app = Flask(__name__)
 
-
 # -- Routes section --
 @app.route('/')
-@app.route('/index')
-def index():
-    return "hello world"
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+@app.route('/result', methods = ['POST'])
+def result():
+    form = request.form
+    length = form['length']
+    length = length.strip() # remove leading & trailing whitespace
+
+    # user did not enter a valid length
+    if not length.isnumeric():
+        return "Error: You did not enter a valid length."
+
+    password = functions.generator(int(length))
+    return render_template('result.html', password = password)
---
+++ functions.py
@@ -0,0 +1,10 @@
+import random
+
+def generator(length):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    password = ""
+
+    for i in range(length):
+        password += random.choice(alphabet)
+    
+    return password
